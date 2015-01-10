e02348ab704b4e7f6c00ba2953ac324764f1b397
Gregory Ditzler <gregory.ditzler@gmail.com>
2015-01-10 17:20:05 -0500

    added basic functionality

--- moa/src/moa-2013.08-sources/moa-src/src/main/java/moa/classifiers/featureselection/OFSL.java
+++ moa/src/moa-2013.08-sources/moa-src/src/main/java/moa/classifiers/featureselection/OFSL.java
@@ -1,6 +1,6 @@
 /*
  *    OFSL.java
- *    Copyright (C) 2014 Drexel University, Philadelphia, PA
+ *    Copyright (C) 2015 Drexel University, Philadelphia, PA
  *    @author Gregory Ditzler (gregory{[dot]}ditzler{[at]}gmail{[dot]}com)
  *
  *    This program is free software; you can redistribute it and/or modify
@@ -58,8 +58,21 @@ public class OFSL extends AbstractClassifier {
 	
 	@Override
 	public double[] getVotesForInstance(Instance inst) {
-		// TODO Auto-generated method stub
-		return null;
+		double[] result = (inst.classAttribute().isNominal()) ? new double[2] : new double[1];
+		double f_t = dot(inst.toDoubleArray(), this.weights);
+		f_t += this.bias;
+		
+		if (inst.classAttribute().isNumeric()) {
+            result[0] = f_t;
+            return result;
+        }
+		if (f_t <= 0) {
+			result[0] = 1;
+		} else {
+			result[1] = 1;
+		}
+		
+		return result;
 	}
 
 	
@@ -72,6 +85,7 @@ public class OFSL extends AbstractClassifier {
 	
 	@Override
 	public void trainOnInstanceImpl(Instance inst) {
+		double y_t;
 		
 		if (this.weights == null) {
 			this.weights = new double[inst.numValues()];
@@ -79,15 +93,32 @@ public class OFSL extends AbstractClassifier {
 				this.weights[i] = 0.0;
 			this.bias = 0.0;
 		}
+		if (inst.classAttribute().isNominal()) {
+			y_t = (inst.classValue() == 0) ? -1 : 1;
+        } else {
+        	y_t = inst.classValue();
+        }
 		
-		double y_t = (inst.classValue() == 0) ? -1 : 1;
 		double f_t = dot(inst.toDoubleArray(), this.weights);
 		f_t += this.bias;
 		
 		if (y_t*f_t < 0){
+			double[] m_weights_p1 = scalar_vector(1.0 - this.stepSizeOption.getValue()*this.learningRateOption.getValue(), this.weights);
+			double m_bias_p1 = (1.0 - this.stepSizeOption.getValue()*this.learningRateOption.getValue())*this.bias;
+			double[] m_weights_p2 = scalar_vector(this.learningRateOption.getValue()*y_t, inst.toDoubleArray());
+			double m_bias_p2 = this.learningRateOption.getValue()*y_t;
+			double[] m_weights = vector_add(m_weights_p1, m_weights_p2);
+			double m_bias = m_bias_p1 + m_bias_p2;
 			
+			m_weights = l2_projection(m_weights, m_bias, this.learningRateOption.getValue());
+			m_weights = truncate(m_weights, this.numSelectOption.getValue());
+			
+			for (int i = 0; i < m_weights_p1.length; i++)
+				this.weights[i] = m_weights[i];
+			this.bias = m_weights[m_weights.length-1];
 		} else {
-			
+			this.weights = scalar_vector(1.0 - this.stepSizeOption.getValue()*this.learningRateOption.getValue(), this.weights);
+			this.bias = (1.0 - this.stepSizeOption.getValue()*this.learningRateOption.getValue())*this.bias;
 		}
 		
 		
@@ -134,34 +165,69 @@ public class OFSL extends AbstractClassifier {
 		return y;
 	}
 	
-	
-	/** Sort the elements of a vector 
-	 * @param x vector to be sorted 
-	 * @return sorted vector
+	/**
+	 * Add two double vectors 
+	 * @param x
+	 * @param y
+	 * @return
 	 */
-	private double[] bubblesort(double[] x, double[] y){
-		boolean flag = true;
-		double t;
-		while(flag){
-			flag = false;
-			for (int j = 0; j < x.length - 1; j++){
-				if (x[j] < x[j + 1]){
-					t = x[j];
-					x[j] = x[j + 1];
-					x[j + 1] = t;
-					flag = true;
-				}
-			}
-		}	
+	public double[] vector_add(double[] x, double[] y) {
+		for (int i = 0; i < x.length; i++) {
+			x[i] += y[i];
+		}
 		return x;
 	}
 	
+	/**
+	 * 
+	 * @param x
+	 * @params bias
+	 * @param lambda
+	 * @return
+	 */
+	public double[] l2_projection(double[] x, double bias, double lambda) {
+		double norm = 0.0;
+		double[] y = new double[x.length+1];
+		
+		for (int i = 0; i < x.length; i++)
+			norm += x[i]*x[i];
+		norm = Math.sqrt(norm + bias*bias);
+		double a = (1/Math.sqrt(lambda))/norm;
+		
+		if (a < 1) {
+			for (int i = 0; i < x.length; i++) 
+				y[i] = a*x[i];
+			y[y.length-1] = a*bias;
+		} else {
+			for (int i = 0; i < x.length; i++) 
+				y[i] = x[i];
+			y[y.length-1] = bias;
+		}
+		return y;
+	}
+	
+	
+	/**
+	 * Keep only the B largest entries in a vector x 
+	 * @param x
+	 * @param B
+	 * @return
+	 */
+	public double[] truncate(double[] x, int B) {
+		int[] sorted_indices = bubblesort_index(x);
+		for (int i = 0; i < x.length - B - 1; i++)
+			x[sorted_indices[i]] = 0.0;
+		return x;
+	}
+	
+	
+	
 	/** Sort the elements of a vector 
 	 * @param x vector to be sorted 
 	 * @return sorted vector
 	 */
-	private double[] bubblesort_index(double[] x){
-		double[] y = new double[x.length];
+	private int[] bubblesort_index(double[] x){
+		int[] y = new int[x.length];
 		boolean flag = true;
 		double t, r;
 		
@@ -180,40 +246,12 @@ public class OFSL extends AbstractClassifier {
 					y[j] = y[j + 1];
 					
 					x[j + 1] = t;
-					y[j + 1] = r;
+					y[j + 1] = (int) r;
 					flag = true;
 				}
 			}
 		}	
 		return y;
-	}
-	
-	
-	/** Compute the sign of the elements in a vector
-	 * @param x vector of doubles
-	 * @return sign of the vectors elements
-	 */
-	private double[] sign(double[] x){
-		for (int j = 0; j < x.length; j++) {
-			if (x[j] >= 0) 
-				x[j] = 1.0;
-			else
-				x[j] = -1.0;
-		}
-		return x;
-	}
-	
-	
-	/** Compute the absolute value of a vector
-	 * @param x input vector
-	 * @return absolute value of the elements in x
-	 */
-	private double[] abs(double[] x){
-		for (int j = 0; j < x.length; j++) {
-			if (x[j] < 0) 
-				x[j] = -x[j];
-		}
-		return x;
 	}
 	
 	
