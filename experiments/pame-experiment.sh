8ba94f38e46ed0183bd278d261395f6a537436c1
Gregory Ditzler <gregory.ditzler@gmail.com>
2016-07-06 15:03:31 -0400

    now weak

--- experiments/pame-experiment.sh
+++ experiments/pame-experiment.sh
@@ -23,8 +23,9 @@ out_fp=${home_fp}/Git/MassiveOnlineAnalysis/experiments/outputs2
 datasets=$(cat ${home_fp}/Git/ClassificationDatasets/two-class-datasets.txt | sed -e 's/^/\/scratch\/ditzler\/Git\/ClassificationDatasets\/arffs\//g' -e 's/$/.arff/g')
 #base_clfr=$(echo "bayes.NaiveBayes")
 #base_short=bayes
-base_clfr=$(echo "trees.HoeffdingTree -b")
-base_short=hoeff
+#base_clfr=$(echo "trees.HoeffdingTree -b")
+base_clfr=$(echo "trees.DecisionStump")
+base_short=weak
 nclfr=100
 
 for dataset in ${datasets[@]}; do 
